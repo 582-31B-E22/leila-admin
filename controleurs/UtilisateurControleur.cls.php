d35e7676727987a08dd7b18d8df96fd510948138
Camille Semaan <csemaan@cmaisonneuve.qc.ca>
2022-07-14 10:41:02 -0400

    Code non-fonctionnel (à la pause)

--- controleurs/UtilisateurControleur.cls.php
+++ controleurs/UtilisateurControleur.cls.php
@@ -10,4 +10,33 @@ class UtilisateurControleur extends Controleur
         //$this->tout($params);
 
     }
+
+    public function connexion()
+    {
+        $courriel = $_POST['uti_courriel'];
+        $mdp = $_POST['uti_mdp'];
+
+        $utilisateur = $this->modele->un($courriel);
+
+        $erreur = false;
+        if(!$utilisateur || !password_verify($mdp, $utilisateur->uti_mdp)) {
+            $erreur = "Combinaison courriel/mot de passe erronée";
+        }
+        else if($utilisateur->uti_confirmation != '') {
+            $erreur = "Compte non confirmé : vérifiez vos courriels";
+        }
+        else if(!$utilisateur->uti_actif) {
+            $erreur = "Compte pas activé : communiquez avec votre administrateur";
+        }
+
+        if(!$erreur) {
+            // Sauvegarder l'état de connexion
+
+            // Rediriger vers categorie/tout
+            Utilitaire::nouvelleRoute('categorie/tout');
+        }
+        else {
+
+        }
+    }
 }
---
+++ modeles/UtilisateurModele.cls.php
@@ -0,0 +1,13 @@
+<?php
+class UtilisateurModele extends AccesBd
+{
+    /**
+     * 
+     */
+    public function un($courriel)
+    {
+        return $this->lireUn("SELECT * FROM utilisateur 
+                                WHERE uti_courriel=:email"
+                        , ['email'=>$courriel]);
+    }
+}
